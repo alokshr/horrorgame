b2e8bc0ea265c24ba665091c1f2f64681a8af2e0
AlokS <55328387+alokshr@users.noreply.github.com>
2022-06-13 14:20:29 -0500

    Add project

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float moveSpeed = 1f;
+    Vector3 velocity;
+    bool isGrounded;
+
+    float gravity = -9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float x = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.forward * x + transform.right * z;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
